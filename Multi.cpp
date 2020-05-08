d219cbc49fb115721911b9527b5c1003817d97c9
nypawar18 <nypawar@gmail.com>
2020-05-08 17:41:54 +0530

    Multi level inheritance

---
+++ Multi.cpp
@@ -0,0 +1,18 @@
+/*
+ * Multi.cpp
+ *
+ *  Created on: 08-May-2020
+ *      Author: Nitin
+ */
+#include<iostream>
+using namespace std;
+#include"Multi.h"
+
+int main(void)
+{
+
+	return 1;
+}
+
+
+
---
+++ Multi.h
@@ -0,0 +1,15 @@
+/*
+ * Multi.h
+ *
+ *  Created on: 08-May-2020
+ *      Author: Nitin
+ */
+
+#ifndef MULTI_H_
+#define MULTI_H_
+
+
+
+
+
+#endif /* MULTI_H_ */
